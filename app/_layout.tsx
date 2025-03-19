6c143bff130660ee151ef1c7e1c7e7d7783e93ec
Yash <yash.krishnan123@gmail.com>
2025-03-19 17:31:24 +0000

    Home Calendar and Settings Page Created

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,33 @@
 import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-export default function RootLayout() {
-  return <Stack />;
+
+
+export default function TabLayout() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: 'blue' }}>
+      <Tabs.Screen
+        name="Home"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name = "Calendar"
+        options = {{
+          title: 'Calendar',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="calendar" color={color} />
+        }}
+      />
+      <Tabs.Screen
+        name="Settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color }) => <FontAwesome size={28} name="cog" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
 }
