5d6a98029a503cbd41e204748a7f47a51d4a98c8
James Thirkeld <jamesthirkeld12345@gmail.com>
2025-03-23 20:04:35 +0000

    Added functionality to section class

--- types/Section.ts
+++ types/Section.ts
@@ -1,5 +1,6 @@
 import { Tracker } from "./Tracker";
 import { TimePeriod } from "./Tracker";
+
 export class Section{
     sectionTitle: string;
     size: number = 0;
@@ -11,10 +12,21 @@ export class Section{
         this.timePeriod = timePeriod;
     }
 
+    //Tracker functions
     addTracker(newTracker: Tracker){
         this.trackers.push(newTracker);
     }
 
-    
+    reorderTracker(tracker: Tracker, newPos : number){
+       const oldPos = this.trackers.indexOf(tracker); //old position
+
+       this.trackers.splice(oldPos, 1); // Remove tracker from old position
+       this.trackers.splice(newPos, 0, tracker); // Insert tracker at new position
+    }
+
+    deleteTracker(tracker: Tracker){
+        const pos = this.trackers.indexOf(tracker); //old position
+        this.trackers.splice(pos, 1);
+    }
 }
 
