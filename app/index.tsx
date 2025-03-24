b6b193a0c3fabf96120c26dde8ea74ccb72d1441
tadams04 <tomsadams@gmail.com>
2025-03-24 00:01:51 +0000

    Added scroll and grids to home screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
-import { View, Alert, Pressable, Text } from "react-native";
-import { Ionicons, MaterialCommunityIcons, AntDesign } from "@expo/vector-icons";
+import { View, Alert, Pressable, Text, ScrollView } from "react-native";
+import { Ionicons, MaterialCommunityIcons, AntDesign, Entypo } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 import * as Progress from "react-native-progress"; 
 
@@ -45,70 +45,183 @@ export default function Index() {
         </Pressable>
       </View>
 
-      <View style={progressStyles.progressContainer}>
-        <Progress.Circle
-          size={100} // Size of the circle
-          progress={0.76} // 76% progress
-          thickness={10} // Border thickness
-          showsText={false} // We’ll add text separately
-          color="lightgreen" // Progress color
-          unfilledColor="#333" // Background color
-          borderWidth={0} // No border
-        />
-        <Text style={progressStyles.progressText}>76%</Text>
-      </View>
+      {/* Used as spaces so content does not cover tl and tr icons */}
+      <View style = {{ height: 35}} />  
+
+      <ScrollView
+      contentContainerStyle={{
+        alignItems: "center",
+        paddingBottom: 50, // gives breathing room for bottom of the content , adjust as we like
+      }}>
+        <View style={progressStyles.progressContainer}>
+          <Progress.Circle
+            size={100} // Size of the circle
+            progress={0.76} // 76% progress
+            thickness={10} // Border thickness
+            showsText={false} // We add text separately
+            color="lightgreen" // Progress color
+            unfilledColor="#333" // Background color
+            borderWidth={0} // No border
+          />
+          <Text style={progressStyles.progressText}>76%</Text>
+        </View>
 
 
-      {/* Title text */}
-      <Text
-        style={{
-          color: "white",
-          fontSize: 24,
-          fontWeight: "bold",
-          marginBottom: 20,
-          marginTop: 50,
-        }}
-      >
-        Goals
-      </Text>
+        {/* Title text */}
+        <Text
+          style={{
+            color: "white",
+            fontSize: 24,
+            fontWeight: "bold",
+            marginBottom: 20,
+            marginTop: 50,
+          }}
+        >
+          Goals
+        </Text>
 
-      {/* Row of action buttons */}
-      <View
-        style={{
+        {/* Row of action buttons */}
+        <View
+          style={{
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            gap: 16,
+            paddingHorizontal: 35,
+          }}
+        >
+          <Pressable
+            onPress={() => Alert.alert("Sleep button pressed")}
+            style={squareIconButtonStyle}
+          >
+            <MaterialCommunityIcons name="power-sleep" size={40} color="white" />
+          </Pressable>
+
+          <Pressable
+            onPress={() => Alert.alert("Food button pressed")}
+            style={squareIconButtonStyle}
+          >
+            <Ionicons name="fast-food-outline" size={40} color="white" />
+          </Pressable>
+
+          <Pressable
+            onPress={() => Alert.alert("Calorie button pressed")}
+            style={squareIconButtonStyle}
+          >
+            <Ionicons name="flame-outline" size={40} color="white" />
+          </Pressable>
+
+          <Pressable
+            onPress={() => Alert.alert("Code button pressed")}
+            style={squareIconButtonStyle}
+          >
+            <AntDesign name="codesquareo" size={30} color="white" />
+          </Pressable>
+
+          <Pressable
+            onPress={() => Alert.alert("Plus button pressed")}
+            style={squareIconButtonStyle}
+          >
+            <AntDesign name="plus" size={30} color="white" />
+          </Pressable>
+          
+        </View>
+
+
+        {/* Title text */}
+        <Text
+          style={{
+            color: "white",
+            fontSize: 24,
+            fontWeight: "bold",
+            marginTop: 50,
+          }}
+        >
+          Limits
+        </Text>
+
+        <View
+        style = {{
           flexDirection: "row",
-          justifyContent: "space-around",
+          flexWrap: "wrap",
+          justifyContent: "flex-start",
           alignItems: "center",
           gap: 16,
+          paddingHorizontal: 35,
+          marginTop: 20,
         }}
-      >
-        <Pressable
-          onPress={() => Alert.alert("Sleep button pressed")}
-          style={squareIconButtonStyle}
         >
-          <MaterialCommunityIcons name="power-sleep" size={40} color="white" />
-        </Pressable>
+          <Pressable
+            onPress={() => Alert.alert("Limit button alert")}
+            style={squareIconButtonStyle}
+          >
+            <Ionicons name="cash-outline" size={40} color="white"/> 
+          </Pressable> 
 
-        <Pressable
-          onPress={() => Alert.alert("Food button pressed")}
-          style={squareIconButtonStyle}
-        >
-          <Ionicons name="fast-food-outline" size={40} color="white" />
-        </Pressable>
+          <Pressable
+            onPress={() => Alert.alert("Button pressed")}
+            style={squareIconButtonStyle}
+          >
+            <MaterialCommunityIcons name="spoon-sugar" size={40} color="white" />
+          </Pressable>
 
-        <Pressable
-          onPress={() => Alert.alert("Calorie button pressed")}
-          style={squareIconButtonStyle}
-        >
-          <Ionicons name="flame-outline" size={40} color="white" />
-        </Pressable>
+          <Pressable
+            onPress={() => Alert.alert("Button pressed")}
+            style={squareIconButtonStyle}
+          >
+            <AntDesign name="dashboard" size={30} color="white" />
+          </Pressable>
 
-        <Pressable
-          onPress={() => Alert.alert("Plus button pressed")}
-          style={squareIconButtonStyle}
-        >
-          <AntDesign name="plus" size={30} color="white" />
-        </Pressable>
-      </View>
+          <Pressable
+            onPress={() => Alert.alert("Limit button alert")}
+            style={squareIconButtonStyle}
+          >
+            <AntDesign name="instagram" size={40} color="white"/> 
+          </Pressable> 
+
+          <Pressable
+            onPress={() => Alert.alert("Button pressed")}
+            style={squareIconButtonStyle}
+          >
+            <Entypo name="area-graph" size={40} color="white" />
+          </Pressable>
+
+          <Pressable
+            onPress={() => Alert.alert("Button pressed")}
+            style={squareIconButtonStyle}
+          >
+            <Entypo name="bowl" size={30} color="white" />
+          </Pressable>
+
+          <Pressable
+            onPress={() => Alert.alert("Limit button alert")}
+            style={squareIconButtonStyle}
+          >
+            <Entypo name="credit" size={40} color="white"/> 
+          </Pressable> 
+
+          <Pressable
+            onPress={() => Alert.alert("Button pressed")}
+            style={squareIconButtonStyle}
+          >
+            <Entypo name="game-controller" size={40} color="white" />
+          </Pressable>
+
+          <Pressable
+            onPress={() => Alert.alert("Button pressed")}
+            style={squareIconButtonStyle}
+          >
+            <AntDesign name="plus" size={30} color="white" />
+          </Pressable>
+
+        </View>
+      </ScrollView>
+      
+
+
+
+      
     </SafeAreaView>
   );
 }
@@ -118,7 +231,7 @@ const progressStyles = {
     alignItems: "center" as const,  // Center align
     justifyContent: "center" as const,
     position: "relative" as const,
-    marginTop: 60,                  // Push it down slightly
+    marginTop: 25,                  // Push it down slightly
     marginBottom: 20,                // Space before next section
   },
   progressText: {
