288a3fe164f0e76169bf6ae73245cadcc91c2051
James Thirkeld <jamesthirkeld12345@gmail.com>
2025-04-20 14:49:29 +0100

    added lastModified to section.ts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,14 +6,26 @@ import { StatusBar } from "react-native";
 import { openDatabase } from "@/storage/sqlite";
 import { useEffect } from "react";
 import { ThemeProvider } from "./ThemeContext";
-import { useTrackerStore } from "@/storage/store";
+import { useSectionStore, useTrackerStore } from "@/storage/store";
 import { exampleTrackers, TimePeriod, Tracker } from "@/types/Tracker";
+import { Section } from "@/types/Section";
 export default function Layout() {
 
   //import methods
   const setTrackers = useTrackerStore((s) => s.setTrackers);
   const addTracker = useTrackerStore((s) => s.addTracker);
-  
+  const setSectionsH= useSectionStore((s) => s.setSectionsH);
+  const addSectionH = useSectionStore((s) => s.addSectionH);
+
+  //types
+  type SectionRow = {
+    section_id: number;
+    section_title: string;
+    time_period: TimePeriod;
+    position: number;
+    last_modified: number;
+  };
+
   type TrackerRow = {
     tracker_id: number;
     tracker_name: string;
@@ -34,12 +46,12 @@ export default function Layout() {
         //Querying
         //ALL trackers
         const trackersInfo: TrackerRow[] = await db.getAllAsync<TrackerRow>("SELECT tracker_id,tracker_name,icon,time_period,unit,bound_amount,current_amount,last_modified FROM trackers");
-        const sectionsInfo = await db.getAllAsync("SELECT section_id,section_title,time_period,position,last_modified FROM sections");
+        const sectionsInfo: SectionRow[] = await db.getAllAsync<SectionRow>("SELECT section_id,section_title,time_period,position,last_modified FROM sections");
         const sectionTrackersInfo = await db.getAllAsync("SELECT section_id,tracker_id,tracker_position FROM section_trackers");
 
         //initializing all trackers first
         // write to Zustand
-        const trackers : Tracker[] = trackersInfo.map(tracker => { //for referencing
+        const trackers : Tracker[] = trackersInfo.map(tracker => { //const mapped for referencing later
           const newTracker = new Tracker(
             tracker.tracker_name,
             tracker.icon,
@@ -52,6 +64,18 @@ export default function Layout() {
           addTracker(newTracker);
           return newTracker;
         });
+
+        /*
+        //initializing all sections next
+        const sections : Section[] = sectionsInfo.map(section => { //const mapped for referencing later
+          const newSection = new Section(
+            section.section_title,
+            tracker.icon,
+            tracker.time_period,
+          );
+          addSectionH(newSection);
+          return newTracker;
+        });*/
       } catch (error) {
         console.error("Database error:",error);
       }
