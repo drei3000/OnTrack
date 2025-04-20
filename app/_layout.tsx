f5460716fe2b55fd23152bdc66b87ce295023709
James Thirkeld <jamesthirkeld12345@gmail.com>
2025-04-20 14:32:59 +0100

    made trackers load from database

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,24 +7,51 @@ import { openDatabase } from "@/storage/sqlite";
 import { useEffect } from "react";
 import { ThemeProvider } from "./ThemeContext";
 import { useTrackerStore } from "@/storage/store";
-import { exampleTrackers } from "@/types/Tracker";
-
+import { exampleTrackers, TimePeriod, Tracker } from "@/types/Tracker";
 export default function Layout() {
 
   //import methods
-  const setTrackers = useTrackerStore((state) => state.setTrackers);
+  const setTrackers = useTrackerStore((s) => s.setTrackers);
+  const addTracker = useTrackerStore((s) => s.addTracker);
+  
+  type TrackerRow = {
+    tracker_id: number;
+    tracker_name: string;
+    icon: string;
+    time_period: TimePeriod;
+    last_modified: number;
+    bound_amount: number;
+    unit?: string;
+    current_amount?: number;
+  };
   
   useEffect(() => { //runs on launch
-    
-
     const setupDatabase = async () => { //function to copy and open database
       try{
-        const db = await openDatabase();
+        const db = await openDatabase();1
         console.log("Database initialized");
 
         //Querying
         //ALL trackers
-        const trackersInfo = await db.runAsync("SELECT tracker_name,icon,time_period,unit,bound_amount,current_amount FROM trackers");
+        const trackersInfo: TrackerRow[] = await db.getAllAsync<TrackerRow>("SELECT tracker_id,tracker_name,icon,time_period,unit,bound_amount,current_amount,last_modified FROM trackers");
+        const sectionsInfo = await db.getAllAsync("SELECT section_id,section_title,time_period,position,last_modified FROM sections");
+        const sectionTrackersInfo = await db.getAllAsync("SELECT section_id,tracker_id,tracker_position FROM section_trackers");
+
+        //initializing all trackers first
+        // write to Zustand
+        const trackers : Tracker[] = trackersInfo.map(tracker => { //for referencing
+          const newTracker = new Tracker(
+            tracker.tracker_name,
+            tracker.icon,
+            tracker.time_period,
+            tracker.last_modified,
+            tracker.bound_amount,
+            tracker.unit,
+            tracker.current_amount ? tracker.current_amount : 0 //shouldnt be undefined
+          );
+          addTracker(newTracker);
+          return newTracker;
+        });
       } catch (error) {
         console.error("Database error:",error);
       }
--- types/Tracker.ts
+++ types/Tracker.ts
@@ -3,21 +3,34 @@ export class Tracker { //export so referencable
     trackerName: string;
     icon: string; // Assuming it's a URL
     timePeriod: TimePeriod; // Enum alternative
-    currentAmount: number = 0;
+    currentAmount: number;
     last_modified: number;
     unit?: string; // Optional field
     bound: number; //- if limit, +if goal, 0 if neither
     
-    constructor(trackerName: string, icon: string, timePeriod: TimePeriod, last_modified: number, bound: number, unit?: string) {
-        this.trackerName = trackerName;
-        this.icon = icon;
-        this.timePeriod = timePeriod;
-        this.last_modified = last_modified;
-        this.unit = unit;
-        this.bound = bound;
-
-        //INSERT INTO A SECTION
-        //INSERT INTO DATABASE
+    constructor(trackerName: string, icon: string, timePeriod: TimePeriod, last_modified: number, bound: number, unit?: string);
+    constructor(trackerName: string, icon: string, timePeriod: TimePeriod, last_modified: number, bound: number,  unit: string | undefined, currentAmount: number,);
+    constructor(
+      trackerName: string,
+      icon: string,
+      timePeriod: TimePeriod,
+      last_modified: number,
+      bound: number,
+      unit?: string,
+      currentAmount?: number,
+    ) {
+      this.trackerName = trackerName;
+      this.icon = icon;
+      this.timePeriod = timePeriod;
+      this.last_modified = last_modified;
+      this.bound = bound;
+      this.currentAmount = currentAmount ? currentAmount : 0;
+      this.unit = unit;
+      
+      
+  
+      // INSERT INTO A SECTION
+      // INSERT INTO DATABASE
     }
 
    
