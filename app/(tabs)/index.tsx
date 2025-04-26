0a269724d9d55c6c9eab30974d3b2545c396e885
James Thirkeld <jamesthirkeld12345@gmail.com>
2025-04-26 18:00:31 +0100

    changed styling

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -866,7 +866,11 @@ export default function Index() {
         >
           <View style={[styles.modalOverlay, { backgroundColor: currentTheme["rgba(0, 0, 0, 0.8)"] }]}>
             <View
-              style={[styles.modalContent, { backgroundColor: currentTheme["101010"] }]}
+              style={[styles.modalContent, { 
+                backgroundColor: currentTheme["101010"],
+                borderColor: currentTheme['dimgray'],
+              }    
+            ]}
             >
               {/* Close button */}
               <Pressable
@@ -912,7 +916,8 @@ export default function Index() {
                     style={[
                       styles.trackerButton,
                       {
-                        borderBottomColor: currentTheme.dimgray,
+                        borderWidth: 1,
+                        borderColor: 'white',
                         backgroundColor: currentTheme["101010"],
                       },
                     ]}
@@ -1018,8 +1023,9 @@ const styles = StyleSheet.create({
   },
   modalContent: {
     width: screenWidth * 0.9,
-    height: "70%",
+    height: 450,
     borderRadius: 10,
+    borderWidth: 1,
     padding: 20,
     alignItems: "center",
     justifyContent: "flex-start",
@@ -1034,7 +1040,7 @@ const styles = StyleSheet.create({
   },
 
   trackerButton: {
-    width: "26%", // Adjust to fit 3 items per row
+    width: "21%", // Adjust to fit 4 items per row
     aspectRatio: 1, // Make it square
     margin: 10, // Add spacing between buttons
     justifyContent: "center",
